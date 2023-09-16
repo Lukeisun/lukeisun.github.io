adcb186f37f9eaab85df4a3d20d06ec45f820079
Adin Lukas <Lukeisun57@gmail.com>
2023-09-16 15:32:20 -0400

    started basic layout

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+
+export default defineConfig({
+  site: 'https://lukeisun.github.io',
+})
+// https://astro.build/config
+// export default defineConfig({
+// 	site: 'https://example.com',
+// 	integrations: [mdx(), sitemap()],
+// });
---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const NAME = 'Adin Lukas DelaCruz'; 
+export const EMAIL = 'adinlukas01@gmail.com';
+export const EMAIL_SVG = './public/email.svg';
+export const CS2 = './public/cs2.webp';
+export const GITHUB = './public/ghdrawn.svg';
+export const ME_JPG = './public/me.jpg';
