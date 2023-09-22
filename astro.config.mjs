60294db48a7b3422a02c99ac51492e4d2f7698f1
lukeisun <lukeisun57@gmail.com>
2023-09-21 23:43:29 -0400

    first blog post!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 
 export default defineConfig({
   site: 'https://lukeisun.github.io',
+  markdown: {
+  },
 })
 // https://astro.build/config
 // export default defineConfig({
