11cb3f1cad7cbf706c5d27ab5dc7cb959bbe79dd
lukeisun <lukeisun57@gmail.com>
2023-12-11 17:58:21 -0500

    changed email

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Adin Lukas DelaCruz';
-export const SITE_DESCRIPTION = 'blog/website thing';
-export const NAME = 'Adin Lukas DelaCruz'; 
-export const EMAIL = 'adinlukas01@gmail.com';
-export const EMAIL_SVG = '/email.svg';
-export const CS2 = '/cs2.webp';
-export const GITHUB = '/ghdrawn.svg';
-export const ME_JPG = '/me.jpg';
+export const SITE_TITLE = "Adin Lukas DelaCruz";
+export const SITE_DESCRIPTION = "blog/website thing";
+export const NAME = "Adin Lukas DelaCruz";
+export const EMAIL = "delacruz@adinlukas.xyz";
+export const EMAIL_SVG = "/email.svg";
+export const CS2 = "/cs2.webp";
+export const GITHUB = "/ghdrawn.svg";
+export const ME_JPG = "/me.jpg";
